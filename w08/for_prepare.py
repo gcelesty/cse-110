e833f0028cf796bcdcd2986d2221211cdf10233f
gcelesty <97373234+gcelesty@users.noreply.github.com>
2022-05-06 14:43:37 -0500

    Create for_prepare.py

---
+++ w08/for_prepare.py
@@ -0,0 +1,19 @@
+# colors = ["red", "blue", "green", "yellow"]
+
+# print()
+# for colors in colors:
+#     print(colors)
+# print()
+
+
+# print()
+# for i in range(1, 9):
+#     print(i)
+# print()
+
+
+
+print()
+for i in range(2, 21, 2):
+    print(i)
+print()
